c7b0ee64d587e9ca37e5f5a430481389ef6c9ebb
Robert Horngren <robert@horngren.net>
2022-05-11 12:21:10 +0200

    added barebone firebase init with trial login

--- App.tsx
+++ App.tsx
@@ -1,11 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import LoginScreen from './src/Screens/LoginScreen';
+
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAzfnZHha_RD20IZQswcwloq1d75n8L8Ig",
+  authDomain: "fuel-app-20474.firebaseapp.com",
+  projectId: "fuel-app-20474",
+  storageBucket: "fuel-app-20474.appspot.com",
+  messagingSenderId: "793154998078",
+  appId: "1:793154998078:web:e70511d2208196461aa66f"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <LoginScreen />
     </View>
   );
 }
---
+++ src/Screens/LoginScreen.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { View, Text, Pressable } from "react-native";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+
+const LoginScreen = () => {
+  const [userName, setUserName] = useState('robert@horngren.net');
+  const [password, setPassword] = useState('1234Aa');
+  const [loginStatus, setLoginStatus] = useState('');
+
+  const auth = getAuth();
+
+  const loginUser = (userName: string, password: string) => {
+    signInWithEmailAndPassword(auth, userName, password)
+      .then((userCredentials) => {
+        console.log('Signed in: ', userCredentials.user.email);
+      })
+      .catch((error) => {
+        console.log('There was an error while signing in: ', error)
+      })
+  }
+
+  return (
+    <View>
+      <Text>Hello from LoginScreen</Text>
+      <Pressable onPress={() => loginUser(userName, password)}>
+        <Text>Login!</Text>
+      </Pressable>
+    </View>
+  )
+}
+
+export default LoginScreen;
