1ec5fb8ba86b1ae23e8e6891a5d3711cbf2fff0b
Robert Horngren <robert@horngren.net>
2022-05-11 15:24:00 +0200

    refined firebase init

---
+++ services/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAzfnZHha_RD20IZQswcwloq1d75n8L8Ig",
+  authDomain: "fuel-app-20474.firebaseapp.com",
+  projectId: "fuel-app-20474",
+  storageBucket: "fuel-app-20474.appspot.com",
+  messagingSenderId: "793154998078",
+  appId: "1:793154998078:web:e70511d2208196461aa66f"
+};
+
+export default firebaseConfig;
