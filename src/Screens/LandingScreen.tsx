a1b913ac5f993b6fecc313981626a709873a5bc1
Robert Horngren <robert@horngren.net>
2022-05-12 12:46:18 +0200

    added persitive login

--- src/Screens/LandingScreen.tsx
+++ src/Screens/LandingScreen.tsx
@@ -4,6 +4,8 @@ import LoginView from '../Components/LoginView';
 import RegistrationView from '../Components/RegistrationView';
 import { Context } from '../context/Context';
 import { LandingScreens, StackScreen } from '../helpers/types';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 
 interface ILandingScreen extends NativeStackScreenProps<StackScreen, "LandingScreen"> { }
 
@@ -13,11 +15,26 @@ const LandingScreen: React.FC<ILandingScreen> = (props) => {
 
   const context = useContext(Context);
 
+  const loadUser = async () => {
+    try {
+      const value = await AsyncStorage.getItem('@authed_User');
+      if (value !== null) {
+        context?.setAuthed(true);
+      }
+    } catch (error) {
+      console.log('There was an error loading user');
+    }
+  }
+
   useEffect(() => {
     if (context?.authed) {
       props.navigation.replace('MainScreen');
     }
   }, [context?.authed]);
+
+  useEffect(() => {
+    loadUser()
+  }, []);
 
   return (
     <Stack.Navigator>
