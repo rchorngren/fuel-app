e675532e6bd4727a8aef9862cf8d9d44fde341fa
Robert Horngren <robert@horngren.net>
2022-05-17 14:09:51 +0200

    Enabled saving of tank data to firebase

--- src/Screens/HomeScreen.tsx
+++ src/Screens/HomeScreen.tsx
@@ -1,8 +1,26 @@
-import React from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import React, { useContext, useEffect } from "react";
 import { View, Text } from "react-native";
+import { Context } from "../context/Context";
 
 
 const HomeScreen = () => {
+  const context = useContext(Context);
+
+  const loadUser = async () => {
+    try {
+      const user = await AsyncStorage.getItem('@authed_User');
+      const parsedUser = JSON.parse(user!);
+      context?.setAuthedUserUid(parsedUser.uid);
+    } catch (error) {
+      console.log('There was an error while loading the user: ', error);
+    }
+  }
+
+
+  useEffect(() => {
+    loadUser();
+  }, []);
 
 
   return (
